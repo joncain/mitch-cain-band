1864c3309ca3a6db4c72ec14c9de8669316ad77c
joncain <jon@joncain.net>
2017-05-11 15:19:22 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function ()
+{
+    $("div.modal-header-close").bind('click', function ()
+    {
+        var p = $(this).parents().get(2);
+        $(p).fadeOut();
+    });
+
+    $("a#about-link").bind('click', function ()
+    {
+        var p = $("div.modal-header-close").parents().get(2);
+        $(p).fadeIn();
+    });
+
+});
